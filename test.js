9e1c282392d1ae30d8a9b2728820d9f5d0140364
Jan Lazarek <honzakerazal@volny.cz>
2017-03-06 15:22:12 +0100

    Tabulky předností
    přidány tabulky popisující přednostní situace na jednotlivých
    křižovatkách

--- test.js
+++ test.js
@@ -1,17 +1,23 @@
 ﻿// alert("Nazdar");
+// generator of car positioning and vectors
 base();
-var a = generatorA();
-//var b = generatorB();
-//var c = generatorC();
-//var d = generatorD();
+
+function base(){
+generatorA();
+generatorB();
+generatorC();
+generatorD();
+}
+
+
 
 function generatorA(){
 	x1 = 3;
-	y1 = 3;
+	y1 = 1;
 	x2 = 0;
 	y2 = 0;
 	
-	x2 = Math.floor(Math.random() * 3);
+	x2 = Math.floor(Math.random() * 4);
 	switch(x2){
   case 1:
     x2 = 1;
@@ -29,29 +35,30 @@ function generatorA(){
 	x2 = 0;
 	y2 = 0;
 	}
-}
-function base(){
-
-generatorA();
-alert(y2);
-}	
+	var a1 = (y2 - y1);
+	var	b1 = (x2 - x1);
+	//c1 = a1 * x1 + b1 * y1
+	
+	console.log(a1, b1);
+	return [a1, b1];
+	}
 
 function generatorB(){
-	x1 = 3;
+	x1 = 4;
 	y1 = 3;
 	x2 = 0;
 	y2 = 0;
 	
 	
-	x2 = Math.floor(Math.random() * 3) + 1;
+	x2 = Math.floor(Math.random() * 4);
 	switch(x2){
   case 1:
     x2 = 1;
 	y2 = 3;
     break;
   case 2:
-    x2 = 4;
-	y2 = 2;
+    x2 = 2;
+	y2 = 1;
     break;	
   case 3:
     x2 = 3;
@@ -61,130 +68,76 @@ function generatorB(){
 	x2 = 0;
 	y2 = 0;
 	}
+	var a2 = y2 - y1;
+	var b2 = x2 - x1;
+	//c1 = a1 * x1 + b1 * y1
+	
+	console.log(a2, b2);
+	return [a2, b2];
 }
 
 function generatorC(){
-	x1 = 3;
-	y1 = 3;
+	x1 = 2;
+	y1 = 4;
 	x2 = 0;
 	y2 = 0;
 	
 	
-	x2 = Math.floor(Math.random() * 3) + 1;
+	x2 = Math.floor(Math.random() * 4);
 	switch(x2){
   case 1:
     x2 = 1;
 	y2 = 3;
     break;
   case 2:
-    x2 = 4;
-	y2 = 2;
+    x2 = 2;
+	y2 = 1;
     break;	
   case 3:
-    x2 = 3;
-	y2 = 4;
+    x2 = 4;
+	y2 = 2;
     break;
 	default:
 	x2 = 0;
 	y2 = 0;
 	}
+	var a3 = y2 - y1;
+	var b3 = x2 - x1;
+	//c1 = a1 * x1 + b1 * y1
+	console.log(a3, b3);
+	return [a3, b3];
 }
 
 function generatorD(){
-	x1 = 3;
-	y1 = 3;
+	x1 = 1;
+	y1 = 2;
 	x2 = 0;
 	y2 = 0;
 	
-	x2 = Math.floor(Math.random() * 3) + 1;
+	x2 = Math.floor(Math.random() * 4);
 	switch(x2){
   case 1:
-    x2 = 1;
-	y2 = 3;
+    x2 = 2;
+	y2 = 1;
     break;
   case 2:
-    x2 = 4;
-	y2 = 2;
-    break;	
-  case 3:
     x2 = 3;
 	y2 = 4;
+    break;	
+  case 3:
+    x2 = 4;
+	y2 = 2;
     break;
 	default:
 	x2 = 0;
 	y2 = 0;
 	}
+	var a4 = y2 - y1;
+	var b4 = x2 - x1;
+	//c1 = a1 * x1 + b1 * y1
+	console.log(a4, b4);
+	return [a4, b4];
 }	
-	
-	
-	/*
-	if(x2 = 1){
-		y2 = 3;
-		return x2;
-		return y2;
-	}else{
-	if(x2 = 2){
-		x2 = 4;
-		y2 = 2;
-		return x2;
-		return y2;
-	}else{
-	if(x2 = 3){
-		y2 = 4;
-		return x2;
-		return y2;
-	}else{
-		return 0;
-	}
-	}
-	}
-}
-*/
-/*
-function generatorB(){
-	var b = [4, 3, x4, y4];
-	x2 = Math.floor(Math.random() * 3);
-	if(x4 = 1){
-		y4 = 3;
-	}
-	if(x4 = 2){
-		y4 = 1;
-	}else{
-		return 0;
-	}
-}
-function generatorC(){
-	var c = [2, 4, x6, y6];
-	x6 = Math.floor(Math.random() * 3);
-	if(x6 = 1){
-		y6 = 3;
-	}
-	if(x6 = 2){
-		y6 = 1;
-	}
-	if(x6 = 3){
-		x6 = 4;
-		y6 = 2;
-	}else{
-	return 0;
-	}
-}
-function generatorD(){
-	var d = [1, 3, x8, y8];
-	x8 = Math.floor(Math.random() * 3);	
-	if(x8 = 1){
-		x8 = 4;
-		y8 = 2;
-		return x8;
-		return y8;
-	}
-	if(x8 = 2){
-		y8 = 1;
-		return x8;
-		return y8;
-	}else{
-		return 0;
-	}
-}
-*/
+
+
 
