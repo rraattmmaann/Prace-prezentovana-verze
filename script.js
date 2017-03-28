ad1c4a5c777285bcca5acb3762a02e210bd41a72
Jan Lazarek <honzakerazal@volny.cz>
2017-03-28 22:32:44 +0200

    3_28 script Generator
    script.js tvorba generatoru pozic + pořadí průjezdu - vyhodnocování
    údají z tabulky

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
 var tableXfree = [
-    [[0],[10],[2],[2,3]],
-    [[3,4],[0],[10],[3]],
-    [[4],[4,1],[0],[10]],
-    [[10],[1],[1,2],[0]],
+    [[0,10],[10,10],[2,10],[2,3]],
+    [[3,4],[0,10],[10,10],[3,10]],
+    [[4,10],[4,1],[0,10],[10,10]],
+    [[10,10],[1,10],[1,2],[0,10]],
 ]
 
 var tableTfree = [
@@ -14,16 +14,29 @@ var tableTfree = [
 ]
 
 function XFGenerator(){
-    var carChoice = [1,2,3,4];
+    var carChoice = [0,1,2,3];
     var a = Math.floor(Math.random() * 4);
     var b = Math.floor(Math.random() * 4);
     var first = tableXfree[a][b];
-   
+    var order = [];
     if( first == 10 || first == 0){
         XFGenerator();
         return;
     }
 console.log(a,b);
-console.log(first);    
+console.log(first); 
+delete carChoice[b];
+console.log(carChoice);
+delete carChoice[first[0]];
+delete carChoice[first[1]];
+console.log(carChoice);
+order.push(b);
+console.log(order);
+/*
+while(carchoice != undefined){
+    
+} 
+*/  
 }
+
 XFGenerator();
