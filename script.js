cddebe73358fecd2b78b6db1c8d1133ae94fc033
Jan Lazarek <honzakerazal@volny.cz>
2017-04-02 16:47:42 +0200

    4_2 Note pořadí průjezdu
    vytváření generátoru pořadí průjezdu

--- script.js
+++ script.js
@@ -24,19 +24,34 @@ function XFGenerator(){
         return;
     }
 console.log(b,a);           //a = y; b = x
-console.log("pole, které pojede jako první", first); 
-delete carChoice[a];
-console.log(carChoice);
-delete carChoice[first[0]];
-delete carChoice[first[1]];
+console.log("pole, které pojede jako první", first);
+var index1 = carChoice.indexOf(a);
+carChoice.splice(index1, 1);
+var index2 = carChoice.indexOf(first[0]);
+carChoice.splice(index2, 1);
+var index3 = carChoice.indexOf(first[1]);
+carChoice.splice(index3,1);
 console.log("jako další může jet auto z řádku", carChoice);
 order.push(a);
 console.log("postupně pojedou", order);
-
-while(carchoice != undefined){
-    
-} 
-
+return carChoice;
 }
+function tryThat(){
+    var x = Math.floor(Math.random() * 4);
+    var y = Math.floor(Math.random() * 4);
+    var tryIt = tableXfree[y][x];
+      console.log(x, y);
+      console.log(tryIt);
 
+        
+}
+var carChoice = [];
 XFGenerator();
+    var x = 0;
+    var y = 0;
+tryThat();
+
+    if(!carChoice.includes(y)){
+       tryThat(); 
+    }
+    console.log(y);
