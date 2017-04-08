8cb1165e1634e33ebb6f9b2a555a9d1bdab5b29e
Jan Lazarek <honzakerazal@volny.cz>
2017-04-09 01:34:20 +0200

    4_9 note Add secondOne(base)
    práce na funkci secondOne() je ještě třeba upravit logiku vybírání v
    případě jízdy aut, která jsou pouze proti sobě, zamyšlení nad tím, zda
    člověk z výhledu bude skutečně do křižovatky přijíždět

--- ascript.js
+++ ascript.js
@@ -39,7 +39,7 @@ function XFGenerator(){
     console.log("může jet jako další řádek", carChoice);
     order.push(a);
     console.log(order);
-    var base = [[order],[carChoice]];
+    var base = [order,carChoice];
     console.log(base);
     return base;
 }
@@ -47,13 +47,32 @@ function XFGenerator(){
 function secondOne(base){
     var carChoice = base[1];
     var order = base[0];
-    console.log(order);
+    console.log("dosavadní pořadí", order);
     console.log(carChoice);
-    if(order[0] = 0){
+    var r1 = 0;
+    if(order.valueOf(order[0]) == 0){
         var r1 = 3;
     }else{
-        var r1 = valueOf(order[1]) + 1;
+        var r1 = order.valueOf(order[0]);
+        r1--;
     }
     console.log(r1);
-       
+    var delR1 = carChoice.indexOf(r1);
+    carChoice.splice(delR1,1);
+    r2 = Math.floor(Math.random()*4);
+    var car2 = tableXfree[r1][r2];
+    console.log(r1);
+    console.log(car2);
+     if(car2[0] == 10){
+         order.push(r1);
+        
+     }else if(car2[0] == 5){
+       var car3 = carChoice.valueOf(carChoice[0]);
+       var car31 = tableXfree.indexOf(tableXfree[][][]
+     }else{
+        order.push(r1);
+     }  
+     console.log(order);
+    // SPLIT
+ //    var base
 }
